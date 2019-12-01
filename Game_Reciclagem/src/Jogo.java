1364168fca1b47fd0cf5b4c0c72d192a8d69c915
Érica Sousa <ericasousa@alu.ufc.br>
2019-12-01 20:59:09 -0300

    upt extra

--- Game_Reciclagem/src/Jogo.java
+++ Game_Reciclagem/src/Jogo.java
@@ -1,5 +1,3 @@
-import javax.swing.JFrame;
-
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -7,11 +5,12 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class Jogo extends JFrame {
 	boolean mousePressionado = false;
-	
+
 	ImageIcon iconFundo = new ImageIcon(getClass().getResource("fundo.jpg"));
 	ImageIcon iconLixeira = new ImageIcon(getClass().getResource("lixeira.png"));
 	ImageIcon iconLixo = new ImageIcon(getClass().getResource("lixo.png"));
@@ -30,54 +29,52 @@ public class Jogo extends JFrame {
 		addMovimentoTeclado();
 		addMovimentoMouse();
 	}
-	
+
 	public void addMovimentoMouse() {
 		new Mover().start();
 		LLixo.addMouseListener(new MouseListener() {
-			
-			public void mouseReleased(MouseEvent arg0) {//Parar de precionar mouse;
+
+			public void mouseReleased(MouseEvent arg0) {// Parar de precionar mouse;
 				mousePressionado = false;
-				
+
 			}
-			
-			public void mousePressed(MouseEvent arg0) {//Precionar mouse;
+
+			public void mousePressed(MouseEvent arg0) {// Precionar mouse;
 				mousePressionado = true;
-				
+
 			}
-			
+
 			public void mouseExited(MouseEvent arg0) {
-				
-				
+
 			}
-			
+
 			public void mouseEntered(MouseEvent arg0) {
-				
-				
+
 			}
-			
+
 			public void mouseClicked(MouseEvent arg0) {
-				
-				
+
 			}
 		});
 	}
-	
-	//Criar Thread;
-	public class Mover extends  Thread{
+
+	// Criar Thread;
+	public class Mover extends Thread {
 		public void run() {
-			while(true) {
+			while (true) {
 				try {
-					sleep(10);//a cada 10 milesimos, a imagem acompanha o mouse;
+					sleep(10);// a cada 10 milesimos, a imagem acompanha o mouse;
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				if(mousePressionado) {
-					Point ponto = getMousePosition();//Pega a coordenada atual do mouse; 
-					LLixo.setBounds(ponto.x-90, ponto.y-90, 180, 180);
+				if (mousePressionado) {
+					Point ponto = getMousePosition();// Pega a coordenada atual do mouse;
+					LLixo.setBounds(ponto.x - 90, ponto.y - 90, 180, 180);
 				}
 			}
 		}
 	}
+
 	public void addMovimentoTeclado() {
 		addKeyListener(new KeyListener() {
 
@@ -102,7 +99,7 @@ public class Jogo extends JFrame {
 				if (e.getKeyCode() == 39) {// Direita
 					posLixoX += 10;
 				}
-				LLixo.setBounds(posLixoX, posLixoY, 180, 180);//Atualiza posição do lixo;
+				LLixo.setBounds(posLixoX, posLixoY, 180, 180);// Atualiza posição do lixo;
 
 			}
 		});
@@ -116,14 +113,17 @@ public class Jogo extends JFrame {
 
 	// Contém configuração da JFrame;
 	public void editarJanela() {
+		setTitle("Recicle");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(900, 529);// Define tamanho da janela;
+		setResizable(false);// Tela não vai ser redimensionada;
 		setLocationRelativeTo(null);// JFrame centralizada;
 		setVisible(true);// JFrame visível;
 		setLayout(null);// Sem layout na janela;
 		add(LLixo);
 		add(LLixeira);
 		add(LFundo);
+
 	}
 
 	public static void main(String[] args) {
